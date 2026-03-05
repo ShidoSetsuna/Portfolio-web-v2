c3f488dfd687bef602344871d4526840074d459f
Valdemar Andreas <vald3060@gmail.com>
2026-03-05 08:01:32 +0100

    Optimized mobile zooming (Was extremely laggy, likely the reason is the crazy amount of blurs I had. Have reduced it significantly on smaller devices and also added a quick fix for the footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import "./footer.scss";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__text">© 2026 My Portfolio. All rights reserved.</p>
+      <p className="footer__text">© 2026 Valdemar Andreas Larsen. All rights reserved.</p>
     </footer>
   );
 }
